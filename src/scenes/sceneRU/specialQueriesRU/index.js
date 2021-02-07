948d03254cdde2c77129be9fc7fa0d38720f7e99
alexeyinn <alexey.i.nn@gmail.com>
2021-02-07 11:41:39 +0300

    bugFix_goodArticleLinkRU

--- src/scenes/sceneRU/specialQueriesRU/index.js
+++ src/scenes/sceneRU/specialQueriesRU/index.js
@@ -3,7 +3,7 @@ const inTheNewsRU = require('./inTheNewsRU');
 const didYouKnowRU = require('./didYouKnowRU');
 const onThisDayRU = require('./onThisDayRU');
 const dayPictureRU = require('./dayPictureRU');
-const goodArticleRU = require('./dayPictureRU');
+const goodArticleRU = require('./goodArticleRU');
 
 module.exports = {
   featuredArticleRU,
